77cc8f541b1eac6c9b6e1987d5689d8ae6a57acc
Seth Bowman <brandonsethbowman@gmail.com>
2024-05-13 09:42:01 -0500

    Add project files.

---
+++ StaticDemoCSharp-47/MathHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemoCSharp_47
+{
+    public static class MathHelper
+    {
+        //static classes have all static members
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Subtract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+    }
+}
---
+++ StaticDemoCSharp-47/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StaticDemoCSharp_47
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var twoPlusTWO = MathHelper.Add(2, 2);
+
+            var roomOne = new Classroom();
+            roomOne.Teacher = "Seth";
+
+            Classroom.Subject = "C#";
+
+            Console.WriteLine(Classroom.Subject);
+
+            var roomTwo = new Classroom();
+            roomTwo.Teacher = "Mack";
+
+            Classroom.Subject = "Java";
+
+            Console.WriteLine(Classroom.Subject);
+
+        }
+    }
+}
